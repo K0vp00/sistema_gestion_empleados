874cb15feb579a89a709601b149dc3bcf360d5b4
diego gomez <diegog.v465@gmail.com>
2024-10-16 23:52:42 -0300

    creacion de clases y atributos (ProyectoEmpleado, registroTiempo, Proyecto)

---
+++ sistema/classes/proyecto_empleado.py
@@ -0,0 +1,6 @@
+class proyectoEmpleado ():
+    def __init__(self, idProyectoEmpleado, idProyecto, idEmpleado):
+        self.__idProyectoEmpleado = idProyectoEmpleado
+        self.__idProyecto = idProyecto
+        self.__idEmpleado = idEmpleado
+        
